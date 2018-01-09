9d3b44fc36bfc4e6ad51a8842338f563c9a4c905
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-01-09 21:26:07 -0200

    creating painel and foto component

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,9 +1,14 @@
+import { PainelModule } from './painel/painel.module';
+import { FotoModule } from './foto/foto.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
