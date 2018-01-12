15673e55d6c3968b63ea65cfad4cc2dc13b1bd7f
Otávio Felipe do Prado <otavio.prado@matera.com>
2018-01-12 13:03:43 -0200

    implementando método de remoção de fotos no FotoService

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -9,13 +9,37 @@ import { FotoComponent } from '../foto/foto.component';
 })
 export class ListagemComponent {
     fotos: FotoComponent[] = [];
+    service: FotoService;
+    mensagem: string = '';
 
     constructor(service: FotoService) {
+        this.service = service;
+
         service.lista()
             .subscribe(fotos => {
                 this.fotos = fotos;
             }, erro => {
                 console.log(erro);
+            });
+    }
+
+    remover(foto: FotoComponent) {
+        console.log("Chamou o método remover()");
+        console.log(foto);
+
+        this.service
+            .remove(foto)
+            .subscribe(() => {
+                let novasFotos = this.fotos.slice(0);
+                let indice = this.fotos.indexOf(foto);
+                novasFotos.splice(indice, 1);
+
+                this.fotos = novasFotos;
+
+                this.mensagem = 'Foto removida com sucesso';
+            }, erro => {
+                console.log(erro);
+                this.mensagem = 'Não foi possível remover a foto';
             });
     }
 }
